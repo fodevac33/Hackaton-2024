fb02302a9b98b5042c287739b2ed15ca1f475f05
fodevac <fodevac@gmail.com>
2024-08-29 11:36:51 -0500

    Core files

---
+++ Api/src/routes/index.ts
@@ -0,0 +1,10 @@
+
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Welcome to the TypeScript API!' });
+});
+
+export default router;
